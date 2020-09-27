51dfaef88e7e54527d2551959485f449e0665534
researcher-h <research.hhh@gmail.com>
2020-09-27 03:01:26 +0100

    Updates

---
+++ precache-manifest.5bc3898ddae7b33ae9ed8174760932a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9cfdda25c455dcecb8dd517f0d0cff",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "289f259be8b20bcab9a4",
+    "url": "/react-deploy/static/css/main.d2730ee5.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289f259be8b20bcab9a4",
+    "url": "/react-deploy/static/js/main.c938aa98.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "b3baf8419dc2293262cd7bf48b7d455d",
+    "url": "/react-deploy/static/media/research_h.b3baf841.jpg"
+  }
+]);
